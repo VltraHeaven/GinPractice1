f277c26816f4fb49c8426b8cbc6c98d18e1e1257
VltraHeaven <julio@vltraheaven.io>
2019-12-04 20:33:21 -0700

    Created slice for filepaths in 'main.go'.

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
@@ -11,15 +12,32 @@ func main() {
 	// Instantiate gin router
 	router := gin.Default()
 
-	router.LoadHTMLFiles("templates/index.html")
+	// Create a slice of filepaths
+	rt := []string{
+		"templates/index.html",
+		"templates/header.html",
+		"templates/footer.html",
+		"templates/menu.html",
+	}
+
+	// Instantiate elements from filepath slice and pass to router
+	idx := rt[0]
+	hdr := rt[1]
+	ftr := rt[2]
+	nv := rt[3]
+
+	router.LoadHTMLFiles(idx, hdr, ftr, nv)
+
+	// Set routes
 	router.GET("/", func(c *gin.Context) {
+
 		//Call HTML method of Context to render a template
 		//Set HTTP status to 200(OK)
 		//Use 'index.html' template
 		//Pass data from that the page uses
-		c.HTML(http.StatusOK, "index.html", gin.H{ "title": "Home Page", },
-			)
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home Page"})
 	})
+
 	// Serve the web application
 	router.Run(":9090")
 }
